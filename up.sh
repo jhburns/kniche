3a6ab3726b6ad481cbcd840f989c406fde01a55c
jhburns <jburns@chapman.edu>
2019-06-23 21:32:48 -0700

    Updating to protect endpoints with basic auth

---
+++ up.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+linkerd install | kubectl apply -f -
+linkerd check
+
+kubectl apply -f linkerd/
+linkerd inject kube-config/ | kubectl apply -f -
+
+kubectl create secret generic mysecret --from-file ./secrets/auth --namespace=whoami
+kubectl create secret generic mysecret --from-file ./secrets/auth --namespace=kube-system
+kubectl create secret generic mysecret --from-file ./secrets/auth --namespace=linkerd
